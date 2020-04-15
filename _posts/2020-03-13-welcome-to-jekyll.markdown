b399f874b10016f78492d36851f687a21e5cf5a7
Simon Schauß <simon.schauss@brickmakers.de>
2020-04-15 09:27:27 +0200

    add mapbox header

--- _posts/2020-03-13-welcome-to-jekyll.markdown
+++
@@ -1,7 +0,0 @@
----
-layout: post
-title:  "Hackathon Seite ist live"
-date:   2020-03-17 15:00:00 +0100
-categories: hackathon update
----
-Hier findet ihr in den kommenden Monaten Infos über unseren BRICKMAKERS Hackathon
---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// mapboxgl
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2NoYXVib2dhIiwiYSI6ImNrN3VuaDhiMDE5NGkzZnF0dHR0dWJsZzAifQ.aC2Z3EG5ETarbmeCP7mXdQ';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/schauboga/ck7uomjjm1k7e1imqlm50hqvl',
+    center: [7.5890, 50.3569],
+    zoom: 9,
+    interactive: false
+});
+
+const mapContainer = document.getElementById('map-container');
+
+const setMapContainerRotation = (event) => {
+    const rotateX = 1 - (event.clientY / document.body.clientHeight) * 2;
+    const rotateY = 1 - (event.clientX / document.body.clientWidth) * 2;
+    mapContainer.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+};
+
+document.addEventListener('mousemove', setMapContainerRotation, true);
+
+// helper
+
+function onResize() {
+    setViewProperties();
+}
+
+function setViewProperties() {
+    const vh = window.innerHeight * 0.01;
+    const vw = window.innerWidth * 0.01;
+    const style = document.documentElement.style;
+    style.setProperty('--vh', `${vh}px`);
+    style.setProperty('--vw', `${vw}px`);
+    style.setProperty('--vmin', `${Math.min(vh, vw)}px`);
+    style.setProperty('--vmax', `${Math.max(vh, vw)}px`);
+}
+
+window.addEventListener('resize', onResize, true);
+
+setViewProperties();
+
